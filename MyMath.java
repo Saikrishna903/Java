34b00e51cfd05645eee39e7fd102db6228601dd7
Saikrishna903 <73385942+Saikrishna903@users.noreply.github.com>
2020-12-04 20:13:10 +0530

    First commit

---
+++ MyMath.java
@@ -0,0 +1,13 @@
+//MyMath class with two methods.
+class MyMath
+{
+	public int addNumbers(int number1, int number2)
+	{
+		return number1 + number2;
+	}
+	public int multiplyTwoNumbers(int number1, int number2)
+	{
+		return number1 * number2;
+	}
+} 
+
